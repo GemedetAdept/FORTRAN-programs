7f43ac9be383a10e5e50a3a10d2971737f1fb9a4
GemedetAdept <vivian.zitek@gmail.com>
2025-12-22 06:19:33 -0700

    Sync sandbox program

--- f90_sandbox/f90_sandbox.f90
+++ f90_sandbox/f90_sandbox.f90
@@ -1,26 +1,19 @@
 PROGRAM sandbox
 IMPLICIT NONE
 
-	! INTEGER :: i
-	! CHARACTER(LEN=20), DIMENSION(4) :: input_line
+	INTEGER :: test
+	WRITE(*,*) test
 
-	! OPEN(2, FILE ="input.txt", STATUS = "old", ACTION = "read")
-	! 	DO i = 1, 5
-	! 		READ(2,*) input_line(i)
-	! 		WRITE(*,*) input_line(i)
-	! 	END DO
-	! CLOSE(2)
-
-	! CHARACTER(LEN=*), PARAMETER :: test = "abcdefg"
-	! WRITE(*,*) test(1:4)
-
-	CHARACTER(LEN=2) :: to_parse
-	INTEGER :: parsed_int
-	INTEGER :: stat
-
-	to_parse = "24"
-	WRITE(*,*) to_parse
-	READ(to_parse, "(I2)") parsed_int
-	WRITE(*,*) parsed_int
+	CALL change(test)
+	WRITE(*,*) test
 
 END PROGRAM sandbox
+
+SUBROUTINE change(int)
+IMPLICIT NONE
+	
+	INTEGER :: int
+	int = int + 1
+	RETURN
+
+END SUBROUTINE change
