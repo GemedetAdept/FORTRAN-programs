4f1bf0e2e28746efc3e9d6a1e7211b1a3a43139f
GemedetAdept <vivian.zitek@gmail.com>
2026-01-19 07:39:26 -0700

    Test file processing

--- .f90_sandbox/f90_sandbox.f90
+++ .f90_sandbox/f90_sandbox.f90
@@ -1,6 +1,26 @@
 PROGRAM sandbox
 IMPLICIT NONE
 
+INTEGER :: file_unit, file_size
+CHARACTER(LEN=:), ALLOCATABLE :: filename
+CHARACTER(LEN=:), ALLOCATABLE :: buffer
+LOGICAL :: file_exists
 
+file_unit = 1
+filename = "input.txt"
+
+INQUIRE(FILE=filename, EXIST=file_exists)
+INQUIRE(FILE=filename, SIZE=file_size)
+
+ALLOCATE(CHARACTER(LEN=file_size) :: buffer)
+
+IF(file_exists .EQV. .TRUE.) THEN
+    OPEN(file_unit, FILE=filename, STATUS="OLD", ACTION="READ")
+    READ(file_unit, "(A)") buffer
+    CLOSE(file_unit, STATUS="KEEP")
+END IF
+
+buffer = TRIM(buffer)
+WRITE(*,*) buffer
 
 END PROGRAM sandbox
